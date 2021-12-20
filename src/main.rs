3caf6faf6a98b03820a605802101872723a8dfb8
buffalo_lv <buffalo_lv@192.168.0.101>
2021-12-20 10:48:38 +0800

    first commit

--- src/main.rs
+++ src/main.rs
@@ -5,26 +5,24 @@ use solana_sdk::bs58;
 fn main() {
     let address = String::from("9axNqFRy75YxUfebcFNdZfw5a2LGEMyNfWcnTG6Ekqgb");
     let address1 = String::from("25zBh6DbBPQmQUGeLMaEfNAASfvxrm1GvVg3bfTZ6NkG");
-    //let num = String::from("1112j");
-
     //let x:i32 = 100;
     //let x_bytes = x.to_be_bytes();
+    //let original_x = i32::from_be_bytes(x_bytes);
 
     let hash = address.from_base58().unwrap();
     let hash1 = address1.from_base58().unwrap();
 
 
     // println!("{}",hex::encode(&hash));
-    println!("{:02x?}", &hash);
-    println!("{:02x?}", &hash1);
-    //println!("{:02x?}", &num);
+    println!("{:02x?}", hash);
+    println!("{:02x?}", hash1);
 
 
     let decoded = bs58::decode("9axNqFRy75YxUfebcFNdZfw5a2LGEMyNfWcnTG6Ekqgb").into_vec().unwrap();
     let encoded = bs58::encode(&decoded).into_string();
 
     println!("{:02x?}", &decoded);
-    println!("{}", &encoded);//9axNqFRy75YxUfebcFNdZfw5a2LGEMyNfWcnTG6Ekqgb
+    assert_eq!(encoded, "9axNqFRy75YxUfebcFNdZfw5a2LGEMyNfWcnTG6Ekqgb");
 
 
     // let input = "090A0B0C";
@@ -33,5 +31,4 @@ fn main() {
 
     // println!("{:?}", decoded);
 
-    //let original_x = i32::from_be_bytes(x_bytes);
 }
